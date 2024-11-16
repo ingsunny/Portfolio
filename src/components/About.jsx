4d8a3094cb530656b7d8eece15f74da9ea65c82d
Sunny Patel <138172377+sunnypatel-dev@users.noreply.github.com>
2024-11-16 13:05:44 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
           Don&apos;t be shy to contact! 👇
         </p>
         <ul className="flex gap-4">
-          <Link to="https://github.com/sunnypatel-koder/" target="_blank">
+          <Link to="https://github.com/sunnypatel-dev/" target="_blank">
             <li>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -51,7 +51,7 @@ const About = () => {
             </li>
           </Link>
           <Link
-            to="https://www.linkedin.com/in/sunny-patel-7654a4251/"
+            to="https://www.linkedin.com/in/sunnypatel-dev/"
             target="_blank"
           >
             <li>
@@ -70,7 +70,7 @@ const About = () => {
           <Link
             to="#"
             onClick={() =>
-              (window.location.href = "mailto:sunnypatel.koder@gmail.com")
+              (window.location.href = "mailto:sunnypatel.dev@outlook.com")
             }
           >
             <li>
@@ -81,7 +81,7 @@ const About = () => {
                 fill="#fff"
                 viewBox="0 0 24 24"
               >
-                <title>sunnypatel.koder@gmail.com</title>
+                <title>sunnypatel.dev@outlook.com</title>
                 <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"></path>
               </svg>
             </li>
@@ -90,7 +90,7 @@ const About = () => {
         <h1 className="text-xl text-[#c1c2c5] font-bold">My Github Stats</h1>
         <div className="text-[#c1c2c5]">
           <GitHubCalendar
-            username="sunnypatel-koder"
+            username="sunnypatel-dev"
             // hideColorLegend
             labels={{
               totalCount: "{{count}} contributions in this year",
